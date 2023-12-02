2ccbe9f1e9f329c85a65370feb9a40186522f00e
andres <8891980@gmail.com>
2023-12-02 16:08:11 +0300

    Added needed classes for working with Dagger, added to Manifest my class "MyApplication"

---
+++ app/src/main/java/com/drox7/myapplication/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.drox7.myapplication.di
+
+import android.app.Application
+import androidx.room.Room
+import com.drox7.myapplication.data.AddItemRepoImpl
+import com.drox7.myapplication.data.AddItemRepository
+import com.drox7.myapplication.data.MainDb
+import com.drox7.myapplication.data.NoteRepoImpl
+import com.drox7.myapplication.data.NoteRepository
+import com.drox7.myapplication.data.ShoppingListImpl
+import com.drox7.myapplication.data.ShoppingListRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideMainDb(app:Application) : MainDb {
+        return Room.databaseBuilder(
+            app,
+            MainDb::class.java,
+            "shop_list_db"
+            ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideShopRepo(db: MainDb) : ShoppingListRepository{
+        return ShoppingListImpl(db.shoppingListDao)
+    }
+    @Provides
+    @Singleton
+    fun provideAddItemRepo(db: MainDb) : AddItemRepository{
+        return AddItemRepoImpl(db.addItemDao)
+    }
+    @Provides
+    @Singleton
+    fun provideNoteRepo(db: MainDb) : NoteRepository{
+        return NoteRepoImpl(db.noteDao)
+    }
+}

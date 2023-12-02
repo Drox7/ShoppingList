b06fbc54bdd59eea5fed2f2e549acee01c946217
andres <8891980@gmail.com>
2023-12-02 14:51:11 +0300

    Added DataBase MainDb, renamed ShoppingListItem

---
+++ app/src/main/java/com/drox7/myapplication/data/MainDb.kt
@@ -0,0 +1,11 @@
+package com.drox7.myapplication.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+@Database(
+    entities = [ShoppingListItem::class, AddItem::class, NoteItem::class],
+    version = 1
+)
+abstract class MainDb :RoomDatabase() {
+    abstract val shoppingListDao : ShoppingListDao
+}
